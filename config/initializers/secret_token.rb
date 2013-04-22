d096f0e7c8aa24fcd83327f4427c7653eb913101
Dominic Dixon <SheerHippo@gmail.com>
2013-04-22 15:34:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twit2tumb::Application.config.secret_token = '0795012cb81617c4a20ff0a8b44ccecf338e7b65427c0aeeff2c8f9c5ef4ae4a27d340ad7c2f59221b309676388c72ea967529a6221ca65b606069ecf685c660'
