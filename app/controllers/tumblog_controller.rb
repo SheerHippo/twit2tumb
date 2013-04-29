6230bca2515056fefd23c74081e1e71e6b6e4578
Dominic Dixon <SheerHippo@gmail.com>
2013-04-29 15:21:39 -0400

    Correct controller error

--- app/controllers/tumblog_controller.rb
+++ app/controllers/tumblog_controller.rb
@@ -6,7 +6,7 @@ require "digest/hmac"
 require "json/pure"
 require "sinatra"
 
-class TumblrController < ApplicationController
+class TumblogController < ApplicationController
 
 	@key      = "Sr6aNdQZveayHCvkCKH0RPN9mULpinm1gTdJPcbXcujOLHawaV"
 	@secret   = "Nr1LgaoTSa9BH8ZlPEonwYC78PnVc1fYdHT0DoswuWQXxGdCzE"
@@ -27,7 +27,7 @@ class TumblrController < ApplicationController
 	end
 
 	def oauth
-		@request_token = TumblrController.consumer.get_request_token(:oauth_callback => @callback)
+		@request_token = TumblogController.consumer.get_request_token(:oauth_callback => @callback)
 		session[:request_token] = @request_token.token
 		session[:request_token_secret] = @request_token.secret
 		#Send to tumblr.com to authorize
@@ -35,19 +35,19 @@ class TumblrController < ApplicationController
 	end
 
 	def callback
-		@request_token = OAuth::RequestToken.new(TumblrController.consumer, session[:request_token], session[:request_token_secret])
+		@request_token = OAuth::RequestToken.new(TumblogController.consumer, session[:request_token], session[:request_token_secret])
 		@access_token = @request_token.get_access_token({:oauth_verifier => params[:oauth_verifier]})
-		tumblr_user = Tumblr.new({:user_id => 5, :oauth_token => @access_token.token, :oauth_secret => @access_token.secret})
+		tumblr_user = Tumblog.new({:user_id => 5, :oauth_token => @access_token.token, :oauth_secret => @access_token.secret})
 		tumblr_user.save
 		redirect_to(root_path)
 	end
 
 	def show
-		@user = Tumblr.find_by_user_id(5)
+		@user = Tumblog.find_by_user_id(5)
 	end
 
 	def post
-		@user = Tumblr.find_by_user_id(5)
+		@user = Tumblog.find_by_user_id(5)
 		client = Tumblr::Client.new(:consumer_key => @key, :consumer_secret => @secret, :oauth_token => @user.oauth_token, :oauth_token_secret => @user.oauth_secret)
 		client.text(:state => "draft", :body => "test")
 		# url = "api.tumblr.com/v2/blog/sheerhippo.tumblr.com/post"
