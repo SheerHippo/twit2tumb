e734a63e68a9a769af22d9f62d127538c603cc8b
Dominic Dixon <SheerHippo@gmail.com>
2013-05-03 20:52:47 -0400

    Correct controller errors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,16 @@
 require 'twitter'
 
 class TweetsController < ApplicationController
+
+	@key      = "HlHSnyfoBG4bF1PT4ugJg"
+	@secret   = "BUfNXBwl7WpqJPCv0sebKOuilQGhRvfJQf6ec3E6GyI"
+	@callback = "http://pure-hamlet-3918.herokuapp.com/twitter/oauth/callback"
+
+	@base_url 			 = "https://api.twitter.com"
+	@request_token_url   = "/oauth/request_token"
+	@access_token_url 	 = "/oauth/access_token"
+	@authorize_token_url = "/oauth/authorize"
+
   def self.consumer
 		OAuth::Consumer.new(@key , @secret, 
 							{:site 				 => @base_url,
@@ -14,19 +24,19 @@ class TweetsController < ApplicationController
 		@request_token = TweetsController.consumer.get_request_token(:oauth_callback => @callback)
 		session[:request_token] = @request_token.token
 		session[:request_token_secret] = @request_token.secret
-		#Send to tumblr.com to authorize
+		#Send to twitter.com to authorize
 		redirect_to @request_token.authorize_url
 	end
 
 	def callback
-		@request_token = OAuth::RequestToken.new(TumblogController.consumer, session[:request_token], session[:request_token_secret])
+		@request_token = OAuth::RequestToken.new(TweetsController.consumer, session[:request_token], session[:request_token_secret])
 		@access_token = @request_token.get_access_token
-		tumblr_user = Tumblog.new({:user_id => 5, :oauth_token => @access_token.token, :oauth_secret => @access_token.secret})
-		tumblr_user.save
+		twitter_user = Tweets.new({:user_id => 5, :oauth_token => @access_token.token, :oauth_secret => @access_token.secret})
+		twitter_user.save
 		redirect_to(root_path)
 	end
 
 	def show
-		@user = Tumblog.find_by_user_id(5)
+		@user = Tweets.find_by_user_id(5)
 	end
 end
